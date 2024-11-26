c3772e3bf91c6038ed2221795ead1f3977334023
JonseyFTW <mr.chadnjones@gmail.com>
2024-11-25 22:56:18 -0600

    vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import sitemap from 'vite-plugin-sitemap';
-import { copyFileSync, writeFileSync } from 'fs';
-import { resolve } from 'path';
+import { copyFileSync, writeFileSync, existsSync } from 'fs';
+
+// Define the content of robots.txt
+const robotsTxt = `User-agent: *
+Allow: /
+
+Sitemap: https://seatyourguests.com/sitemap.xml`;
+
+// Create robots.txt if it doesn't exist
+const robotsPath = path.resolve(__dirname, 'robots.txt');
+if (!existsSync(robotsPath)) {
+  writeFileSync(robotsPath, robotsTxt);
+}
 
 export default defineConfig({
   plugins: [
     react(),
+    // Integrate vite-plugin-sitemap
     sitemap({
       hostname: 'https://seatyourguests.com',
       dynamicRoutes: [
@@ -20,12 +33,14 @@ export default defineConfig({
       outDir: './dist',
       filename: 'sitemap.xml'
     }),
+    // Plugin to copy robots.txt to the dist folder after build
     {
       name: 'copy-robots',
       writeBundle() {
-        copyFileSync('robots.txt', 'dist/robots.txt');
+        copyFileSync(robotsPath, path.resolve(__dirname, 'dist/robots.txt'));
       },
     },
+    // Plugin to configure server and preview server middleware
     {
       name: 'configure-server',
       configureServer(server) {
